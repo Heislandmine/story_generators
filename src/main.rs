9c79dce09fb94b26c5a95c74f9145660f795a3f1
heislandmine <heislandmine666@protonmail.com>
2024-10-17 06:00:05 +0900

    テストプログラムを追加

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,12 @@
+use generators::base_female_generator::generate_base_female;
+
 mod generators;
 fn main() {
-    println!("Hello, world!");
+    let female = generate_base_female();
+    println!("身長:{}", female.height);
+    println!("体重:{}", female.wight);
+    println!("バスト:{}", female.bust);
+    println!("アンダーバスト:{}", female.under_bust);
+    println!("ウエスト:{}", female.waist);
+    println!("ヒップ:{}", female.hip);
 }
