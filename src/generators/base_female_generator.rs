f76be7617a3878f8254e5422c2332a955fc6103e
heislandmine <heislandmine666@protonmail.com>
2024-10-17 05:49:52 +0900

    generate_base_femaleが正規分布から値をサンプリングするように修正

--- src/generators/base_female_generator.rs
+++ src/generators/base_female_generator.rs
@@ -1,44 +1,73 @@
+use rand_distr::{Distribution, Normal};
+
+
+
 // 定数
-const BASE_FEMALE_MEAN_HEIGHT: i32 = 1569;
-const BASE_FEMALE_HEIGHT_STD: i32 = 68;
-const BASE_FEMALE_MEAN_WIGHT: i32 = 52;
-const BASE_FEMALE_WIGHT_STD: i32 = 7;
-const BASE_FEMALE_MEAN_BUST: i32 = 845;
-const BASE_FEMALE_BUST_STD: i32 = 62;
-const BASE_FEMALE_MEAN_UNDER_BUST: i32 = 718;
-const BASE_FEMALE_UNDER_BUST_STD: i32 = 51;
-const BASE_FEMALE_MEAN_WAIST: i32 = 664;
-const BASE_FEMALE_WAIST_STD: i32 = 72;
-const BASE_FEMALE_MEAN_HIP: i32 = 897;
-const BASE_FEMALE_HIP_STD: i32 = 45;
+const BASE_FEMALE_MEAN_HEIGHT: f32 = 1569.0;
+const BASE_FEMALE_HEIGHT_STD: f32 = 68.0;
+const BASE_FEMALE_MEAN_WIGHT: f32 = 52.0;
+const BASE_FEMALE_WIGHT_STD: f32 = 7.0;
+const BASE_FEMALE_MEAN_BUST: f32 = 845.0;
+const BASE_FEMALE_BUST_STD: f32 = 62.0;
+const BASE_FEMALE_MEAN_UNDER_BUST: f32 = 718.0;
+const BASE_FEMALE_UNDER_BUST_STD: f32 = 51.0;
+const BASE_FEMALE_MEAN_WAIST: f32 = 664.0;
+const BASE_FEMALE_WAIST_STD: f32 = 72.0;
+const BASE_FEMALE_MEAN_HIP: f32 = 897.0;
+const BASE_FEMALE_HIP_STD: f32 = 45.0;
 
 pub struct BaseFemale {
-    pub height: i32,
-    pub wight: i32,
-    pub bust: i32,
-    pub under_bust: i32,
-    pub waist: i32,
-    pub hip: i32,
+    pub height: f32,
+    pub wight: f32,
+    pub bust: f32,
+    pub under_bust: f32,
+    pub waist: f32,
+    pub hip: f32,
 }
 
 pub fn generate_base_female() -> BaseFemale {
-    BaseFemale {
-        height: BASE_FEMALE_MEAN_HEIGHT,
-        wight: BASE_FEMALE_MEAN_WIGHT,
-        bust: BASE_FEMALE_MEAN_BUST,
-        under_bust: BASE_FEMALE_MEAN_UNDER_BUST,
-        waist: BASE_FEMALE_MEAN_WAIST,
-        hip: BASE_FEMALE_MEAN_HIP
+    let waist = sample_from_normal(BASE_FEMALE_MEAN_WAIST, BASE_FEMALE_WAIST_STD);
+
+    let mut under_bust = sample_from_normal(BASE_FEMALE_MEAN_UNDER_BUST, BASE_FEMALE_UNDER_BUST_STD);
+    while waist > under_bust {
+        under_bust = sample_from_normal(BASE_FEMALE_MEAN_UNDER_BUST, BASE_FEMALE_UNDER_BUST_STD);
     }
+
+    let mut bust = sample_from_normal(BASE_FEMALE_MEAN_BUST, BASE_FEMALE_BUST_STD);
+    while waist > bust || under_bust > bust {
+        bust = sample_from_normal(BASE_FEMALE_MEAN_BUST, BASE_FEMALE_BUST_STD);
+    }
+
+    let mut hip = sample_from_normal(BASE_FEMALE_MEAN_HIP, BASE_FEMALE_HIP_STD);
+    while waist > hip || under_bust > hip {
+        hip = sample_from_normal(BASE_FEMALE_MEAN_HIP, BASE_FEMALE_HIP_STD);
+    }
+
+    BaseFemale {
+        height: sample_from_normal(BASE_FEMALE_MEAN_HEIGHT, BASE_FEMALE_HEIGHT_STD),
+        wight: sample_from_normal(BASE_FEMALE_MEAN_WIGHT, BASE_FEMALE_WIGHT_STD),
+        bust,
+        under_bust,
+        waist,
+        hip: hip
+    }
+}
+
+pub fn sample_from_normal(mean: f32, std_dev: f32) -> f32 {
+    let normal = Normal::new(mean, std_dev).unwrap();
+
+    normal.sample(&mut rand::thread_rng())
 }
 
 #[test]
 fn test_generate_base_female() {
-    let result = generate_base_female();
+    for _ in 0..100 {
+        let result = generate_base_female();
 
-    assert!(result.waist < result.under_bust);
-    assert!(result.waist < result.bust);
-    assert!(result.waist < result.hip);
-    assert!(result.under_bust < result.bust);
-    assert!(result.under_bust < result.hip);
+        assert!(result.waist < result.under_bust);
+        assert!(result.waist < result.bust);
+        assert!(result.waist < result.hip);
+        assert!(result.under_bust < result.bust);
+        assert!(result.under_bust < result.hip);
+    }
 }
