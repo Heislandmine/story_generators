a70757cbcd2b8701679154cd8494b2df519842e2
heislandmine <heislandmine666@protonmail.com>
2024-10-21 16:15:20 +0900

    binディレクトリの導入

--- src/bin/gen_female.rs
+++ src/bin/gen_female.rs
@@ -1,6 +1,4 @@
-use generators::base_female_generator::{generate_base_female, BaseFemale};
-
-mod generators;
+use story_generaters::generators::base_female_generator::{generate_base_female, BaseFemale};
 
 fn display_header() {
     let header = "身長,体重,バスト,アンダーバスト,カップサイズ,ウエスト,ヒップ";
@@ -8,10 +6,18 @@ fn display_header() {
 }
 
 fn display_base_female(base_female: &BaseFemale) {
-    println!("{},{},{},{},{},{},{}", base_female.height, base_female.wight, base_female.bust, base_female.under_bust, base_female.cup_size(), base_female.waist, base_female.hip);
+    println!(
+        "{},{},{},{},{},{},{}",
+        base_female.height,
+        base_female.wight,
+        base_female.bust,
+        base_female.under_bust,
+        base_female.cup_size(),
+        base_female.waist,
+        base_female.hip
+    );
 }
 fn main() {
-
     display_header();
 
     for _ in 0..1000 {
